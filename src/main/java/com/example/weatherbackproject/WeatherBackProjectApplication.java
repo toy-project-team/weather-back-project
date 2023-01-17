7a44e9009cfcbb46d351d0891103b8a9a3e9b4f4
choiseon <com1368@naver.com>
2023-01-17 17:33:09 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/weatherbackproject/WeatherBackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherbackproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBackProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherBackProjectApplication.class, args);
+    }
+
+}
