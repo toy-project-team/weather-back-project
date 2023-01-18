6f0775af93164883cfacf12d2f03babe1c031420
choiseon <com1368@naver.com>
2023-01-18 20:19:20 +0900

    feat : 깃 수정

---
+++ src/main/java/com/example/weatherbackproject/scheduler/WeatherApiScheduler.java
@@ -0,0 +1,11 @@
+package com.example.weatherbackproject.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class WeatherApiScheduler {
+
+
+}
---
+++ src/test/java/com/example/weatherbackproject/WeatherBackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherbackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBackProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
