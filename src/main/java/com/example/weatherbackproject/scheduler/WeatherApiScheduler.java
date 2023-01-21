6a6f5246fbf5d7fd20312622db7c258711262a59
choiseon <com1368@naver.com>
2023-01-21 21:31:45 +0900

    feat : 중기강수 예보 생성 스케쥴 구현

--- src/main/java/com/example/weatherbackproject/scheduler/WeatherApiScheduler.java
+++ src/main/java/com/example/weatherbackproject/scheduler/WeatherApiScheduler.java
@@ -1,11 +1,25 @@
 package com.example.weatherbackproject.scheduler;
 
+import com.example.weatherbackproject.service.MidWeatherApiService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @Component
 public class WeatherApiScheduler {
 
+    private final MidWeatherApiService midWeatherApiService;
 
+    public WeatherApiScheduler(MidWeatherApiService midWeatherApiService) {
+        this.midWeatherApiService = midWeatherApiService;
+    }
+
+    @Scheduled(cron = "0 0 6 * * *")
+    public void createMidRainAndCloud() {
+        midWeatherApiService.requestMidLandFcst(LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE) + "0600");
+    }
 }
